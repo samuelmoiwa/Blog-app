534ead50e1eef6adb4b7d46afc0c0eb92e913a3d
samuelmoiwa <samuelmoiwa@gmail.com>
2023-02-09 11:43:23 +0000

    update user_index_spec.rb

--- spec/views/user_index_spec.rb
+++ spec/views/user_index_spec.rb
@@ -1,22 +1,29 @@
 require_relative '../rails_helper'
 RSpec.describe 'index page', type: :system do
+  describe 'User Index' do
   before(:each) do
     @user = User.create(name: 'VITS', photo: 'https:123', bio: 'A software developer based in Kenya', posts_counter: 0)
     3.times do |a|
-      @user.posts.create(author_id: @user.id, title: "#{a} Post")
+      Post.create(author_id: @user.id, title: "#{a} Post")
     end
+    visit users_path
   end
   context 'RENDER USER INDEX' do
     it 'show all content' do
-      visit root_path
       expect(page).to have_content(@user.name)
       expect(page).to have_content(@user.posts_counter)
       expect(page).to have_xpath("//img[@src='#{@user.photo}']")
     end
     it 'redirect to show' do
-      visit root_path
       click_link @user.name, match: :first
-      expect(page).to have_current_path(root_path(@user))
+      expect(page).to have_current_path(user_path(@user))
+    end
+    it 'Should show  the username of all other users' do
+      expect(page).to have_content(@user.name)
+    end
+    it 'Should show the profile picture for each user. ' do
+      expect(page).to have_xpath("//img[@src='#{@user.photo}']")
     end
   end
+end
 end
