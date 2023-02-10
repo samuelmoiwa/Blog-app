26d728194d1ae5b519461899f50359068fe57021
samuelmoiwa <samuelmoiwa@gmail.com>
2023-02-10 11:30:05 +0000

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # root 'users#index'
   # get '/users/:id', to: 'users#show'
   # get 'users/:id/posts', to: 'posts#index'
   # get 'users/:id/posts/:post_id', to: 'posts#show'
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_scope :user do
+    get '/users/sign_in', to: 'users/sessions#new'
+    get '/users/sign_out', to: 'users/sessions#destroy'
+  end
 
   resources :users, only: [:index, :show] do
     resources :posts, only: [:new, :create, :index, :show, ] do
